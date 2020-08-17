8c902f3842e84f6963fd26af18969d7ef53b441b
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2020-08-17 14:50:20 -0700

    privacy policy content

--- src/pages/privacypolicy.js
+++ src/pages/privacypolicy.js
@@ -158,6 +158,174 @@ const PrivacyPolicy = () => {
                                 Use of Your Personal Data
                             </h2>
                         </div>
+
+                        <div className="col-3-of-4">
+                            <p className="paragraph">
+                                The Company may use Personal Data for the following purposes:
+                            </p>
+                            <p className="paragraph paragraph--privacy">
+                                <ul>
+                                    <li><b>To provide and maintain our Service</b>, including to monitor usage of our Service.</li>
+                                    <li><b>To manage Your Account:</b> to manage Your registration as a user of the Service. The Personal Data You provide can give You access to different functionalities of the Service that are available to You as a registered user.</li>
+                                    <li><b>For the performance of a contract</b> the development, compliance and undertaking of the purchase contract for the products, items or services You have purchased or of any other contract with Us through the Service.</li>
+                                    <li><b>To contact You:</b> To contact You by email, telephone calls, SMS, or other equivalent forms of electronic communication, such as a mobile application's push notifications regarding updates or informative communications related to the functionalities, products or contracted services, including the security updates, when necessary or reasonable for their implementation.</li>
+                                    <li><b>To provide You</b> with news, special offers and general information about other goods, services and events which we offer that are similar to those that you have already purchased or enquired about unless You have opted not to receive such information.</li>
+                                    <li><b>To manage Your requests</b> To attend and manage Your requests to Us.</li>
+                                </ul>
+                            </p>
+                            <p className="paragraph">
+                                We may share your personal information in the following situations:
+                            </p>
+                            <p className="paragraph paragraph--privacy">
+                                <ul>
+                                    <li><b>With Service Providers:</b> We may share Your personal information with Service Providers to monitor and analyze the use of our Service, to contact You.</li>
+                                    <li><b>For Business transfers:</b> We may share or transfer Your personal information in connection with, or during negotiations of, any merger, sale of Company assets, financing, or acquisition of all or a portion of our business to another company.</li>
+                                    <li><b>With affiliates:</b> We may share Your information with Our affiliates, in which case we will require those affiliates to honor this Privacy Policy. Affiliates include Our parent company and any other subsidiaries, joint venture partners or other companies that We control or that are under common control with Us.</li>
+                                    <li><b>With Business partners:</b> We may share Your information with Our business partners to offer You certain products, services or promotions.</li>
+                                    <li><b>With other users:</b> when You share personal information or otherwise interact in the public areas with other users, such information may be viewed by all users and may be publicly distributed outside. If You interact with other users or register through a Third-Party Social Media Service, Your contacts on the Third-Party Social Media Service may see Your name, profile, pictures and description of Your activity. Similarly, other users will be able to view descriptions of Your activity, communicate with You and view Your profile.</li>
+                                </ul>
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <h2 className="heading-secondary heading-secondary-privacy">
+                                Retention of Your Personal Data
+                            </h2>
+                        </div>
+
+                        <div className="col-3-of-4">
+                            <p className="paragraph">
+                                The Company will retain Your Personal Data only for as long as is necessary for the purposes set out in this Privacy Policy. We will retain and use Your Personal Data to the extent necessary to comply with our legal obligations (for example, if we are required to retain your data to comply with applicable laws), resolve disputes, and enforce our legal agreements and policies.
+                            </p>
+                            <p className="paragraph">
+                                The Company will also retain Usage Data for internal analysis purposes. Usage Data is generally retained for a shorter period of time, except when this data is used to strengthen the security or to improve the functionality of Our Service, or We are legally obligated to retain this data for longer time periods.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <h2 className="heading-secondary heading-secondary-privacy">
+                                Transfer of Your Personal Data
+                            </h2>
+                        </div>
+
+                        <div className="col-3-of-4">
+                            <p className="paragraph">
+                                Your information, including Personal Data, is processed at the Company's operating offices and in any other places where the parties involved in the processing are located. It means that this information may be transferred to — and maintained on — computers located outside of Your state, province, country or other governmental jurisdiction where the data protection laws may differ than those from Your jurisdiction.
+                            </p>
+                            <p className="paragraph">
+                                Your consent to this Privacy Policy followed by Your submission of such information represents Your agreement to that transfer.
+                            </p>
+                            <p className="paragraph">
+                                The Company will take all steps reasonably necessary to ensure that Your data is treated securely and in accordance with this Privacy Policy and no transfer of Your Personal Data will take place to an organization or a country unless there are adequate controls in place including the security of Your data and other personal information.
+                            </p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <h2 className="heading-secondary heading-secondary-privacy">
+                                Disclosure of Your Personal Data
+                            </h2>
+                        </div>
+
+                        <div className="col-3-of-4">
+                            <h3 className="heading-tertiary heading-tertiary-privacy">Business Transactions</h3>
+                            <p className="paragraph">If the Company is involved in a merger, acquisition or asset sale, Your Personal Data may be transferred. We will provide notice before Your Personal Data is transferred and becomes subject to a different Privacy Policy.</p>
+                            
+                            <h3 className="heading-tertiary heading-tertiary-privacy">Law Enforcement</h3>
+                            <p className="paragraph">Under certain circumstances, the Company may be required to disclose Your Personal Data if required to do so by law or in response to valid requests by public authorities (e.g. a court or a government agency).</p>
+
+                            <h3 className="heading-tertiary heading-tertiary-privacy">Other legal requirements</h3>
+                            <p className="paragraph">The Company may disclose Your Personal Data in the good faith belief that such action is necessary to:</p>
+
+                            <p className="paragraph paragraph--privacy">
+                                <ul>
+                                    <li>Comply with a legal obligation</li>
+                                    <li>Protect and defend the rights or property of the Company</li>
+                                    <li>Prevent or investigate possible wrongdoing in connection with the Service</li>
+                                    <li>Protect the personal safety of Users of the Service or the public</li>
+                                    <li>Protect against legal liability</li>
+                                </ul>
+                            </p>
+
+                            <h3 className="heading-tertiary heading-tertiary-privacy">Security of Your Personal Data</h3>
+                            <p className="paragraph">The security of Your Personal Data is important to Us, but remember that no method of transmission over the Internet, or method of electronic storage is 100% secure. While We strive to use commercially acceptable means to protect Your Personal Data, We cannot guarantee its absolute security.</p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <h2 className="heading-secondary heading-secondary-privacy">
+                                Your California Privacy Rights
+                            </h2>
+                        </div>
+
+                        <div className="col-3-of-4">
+                            <p className="paragraph">Under California Civil Code Section 1798 (California's Shine the Light law), California residents with an established business relationship with us can request information once a year about sharing their Personal Data with third parties for the third parties' direct marketing purposes.</p>
+                            <p className="paragraph">If you'd like to request more information under the California Shine the Light law, and if you are a California resident, You can contact Us using the contact information provided below.</p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <h2 className="heading-secondary heading-secondary-privacy">
+                                California Privacy Rights for Minor Users
+                            </h2>
+                        </div>
+
+                        <div className="col-3-of-4">
+                            <p className="paragraph">California Business and Professions Code section 22581 allow California residents under the age of 18 who are registered users of online sites, services or applications to request and obtain removal of content or information they have publicly posted.</p>
+                            <p className="paragraph">To request removal of such data, and if you are a California resident, You can contact Us using the contact information provided below, and include the email address associated with Your account.</p>
+                            <p className="paragraph">Be aware that Your request does not guarantee complete or comprehensive removal of content or information posted online and that the law may not permit or require removal in certain circumstances.</p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <h2 className="heading-secondary heading-secondary-privacy">
+                                Links to Other Websites
+                            </h2>
+                        </div>
+
+                        <div className="col-3-of-4">
+                            <p className="paragraph">Our Service may contain links to other websites that are not operated by Us. If You click on a third party link, You will be directed to that third party's site. We strongly advise You to review the Privacy Policy of every site You visit.</p>
+                            <p className="paragraph">We have no control over and assume no responsibility for the content, privacy policies or practices of any third party sites or services.</p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <h2 className="heading-secondary heading-secondary-privacy">
+                                Changes to this Privacy Policy
+                            </h2>
+                        </div>
+
+                        <div className="col-3-of-4">
+                            <p className="paragraph">We may update our Privacy Policy from time to time. We will notify You of any changes by posting the new Privacy Policy on this page.</p>
+                            <p className="paragraph">We will let You know via email and/or a prominent notice on Our Service, prior to the change becoming effective and update the "Last updated" date at the top of this Privacy Policy.</p>
+                            <p className="paragraph">You are advised to review this Privacy Policy periodically for any changes. Changes to this Privacy Policy are effective when they are posted on this page.</p>
+                        </div>
+                    </div>
+
+                    <div className="row">
+                        <div className="col-1-of-4">
+                            <h2 className="heading-secondary heading-secondary-privacy">
+                                Contact Us
+                            </h2>
+                        </div>
+
+                        <div className="col-3-of-4">
+                            <p className="paragraph">If you have any questions about this Privacy Policy, You can contact us:</p>
+                            <p className="paragraph paragraph--privacy">
+                                <ul>
+                                    <li>By visiting this page on our website: <a href="http://www.empire534.la/contact">http://www.empire534.la/contact</a></li>
+                                </ul>
+                            </p>
+                        </div>
                     </div>
                 </section>
             </main>
