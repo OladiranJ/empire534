4dccd8bb5f44a3330d40a59277f83558f39d88df
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2020-08-20 11:25:08 -0700

    training page adjustments

--- src/pages/training.js
+++ src/pages/training.js
@@ -35,17 +35,17 @@ const Training = () => {
                     </div>
 
                     <div className="row">
-                        <p className="paragraph u-center-text">
+                        <p className="paragraph u-center-text u-bigger-text">
                             4 week session (8 classes): $320 ($40/class)
                             (3 person minimum, 6 person maximum)
                         </p>
-                        <p className="paragraph u-center-text">
+                        <p className="paragraph u-center-text u-bigger-text">
                             The small groups run in 4 week sessions starting at the beginning of each month. 
                         </p>
-                        <p className="paragraph u-center-text">
+                        <p className="paragraph u-center-text u-bigger-text">
                             You can join at any time during the 4 week session at a prorated rate: $40 X # of Classes Remaining in Session.
                         </p>
-                        <p className="paragraph u-center-text">
+                        <p className="paragraph u-center-text u-bigger-text">
                             Feel free to jump in with an existing group or create your own group.
                         </p>
                     </div>
@@ -61,13 +61,13 @@ const Training = () => {
                     </div>
 
                     <div className="row">
-                        <p className="paragraph paragraph-white u-center-text">
+                        <p className="paragraph paragraph-white u-center-text u-bigger-text">
                             1 Session: $115
                         </p>
-                        <p className="paragraph paragraph-white u-center-text">
+                        <p className="paragraph paragraph-white u-center-text u-bigger-text">
                             5 Pack: $550
                         </p>
-                        <p className="paragraph paragraph-white u-center-text">
+                        <p className="paragraph paragraph-white u-center-text u-bigger-text">
                             10 Pack: $1000
                         </p>
                     </div>
@@ -83,13 +83,13 @@ const Training = () => {
                     </div>
 
                     <div className="row">
-                        <p className="paragraph u-center-text">
+                        <p className="paragraph u-center-text u-bigger-text">
                             1 Session: $140
                         </p>
-                        <p className="paragraph u-center-text">
+                        <p className="paragraph u-center-text u-bigger-text">
                             5 Pack: $650
                         </p>
-                        <p className="paragraph u-center-text">
+                        <p className="paragraph u-center-text u-bigger-text">
                             10 Pack: $1100
                         </p>
                     </div>
