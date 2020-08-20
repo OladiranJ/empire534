6e537ead81b0708c8b3b2245aae899148533a731
Diran Johnson <diranjohnson@Dirans-MacBook-Pro-2.local>
2020-08-19 23:52:15 -0700

    responsive photos part 1

--- src/pages/training.js
+++ src/pages/training.js
@@ -35,8 +35,18 @@ const Training = () => {
                     </div>
 
                     <div className="row">
-                        <p className="paragraph">
-                            Cupidatat enim sit adipisicing voluptate reprehenderit mollit amet. Ex do ipsum aliqua sit aute eu aute anim ut. Enim consectetur culpa ipsum laborum veniam cupidatat adipisicing magna cupidatat reprehenderit aliqua. Exercitation aliqua dolore magna incididunt consectetur aliqua non quis veniam elit aliqua aute nulla.
+                        <p className="paragraph u-center-text">
+                            4 week session (8 classes): $320 ($40/class)
+                            (3 person minimum, 6 person maximum)
+                        </p>
+                        <p className="paragraph u-center-text">
+                            The small groups run in 4 week sessions starting at the beginning of each month. 
+                        </p>
+                        <p className="paragraph u-center-text">
+                            You can join at any time during the 4 week session at a prorated rate: $40 X # of Classes Remaining in Session.
+                        </p>
+                        <p className="paragraph u-center-text">
+                            Feel free to jump in with an existing group or create your own group.
                         </p>
                     </div>
                 </section>
@@ -45,17 +55,20 @@ const Training = () => {
                     <div className="row">
                         <div class="u-center-text u-margin-bottom-big">
                             <h2 class="heading-secondary-white">
-                                Private Training
+                                Personal One-on-One Training
                             </h2>
                         </div>
                     </div>
 
                     <div className="row">
-                        <p className="paragraph paragraph-white">
-                            Tempor aute commodo incididunt dolor culpa irure nulla nisi velit consectetur. Deserunt sit non irure dolore minim exercitation pariatur. Excepteur in sint cupidatat velit cillum sunt sunt consequat. Id qui est adipisicing do consequat labore consectetur deserunt id fugiat et irure proident aute. Consequat incididunt labore Lorem labore adipisicing fugiat esse qui excepteur. Occaecat ipsum labore dolor excepteur duis mollit occaecat.
+                        <p className="paragraph paragraph-white u-center-text">
+                            1 Session: $115
                         </p>
-                        <p className="paragraph paragraph-white">
-                            Tempor aute commodo incididunt dolor culpa irure nulla nisi velit consectetur. Deserunt sit non irure dolore minim exercitation pariatur. Excepteur in sint cupidatat velit cillum sunt sunt consequat. Id qui est adipisicing do consequat labore consectetur deserunt id fugiat et irure proident aute. Consequat incididunt labore Lorem labore adipisicing fugiat esse qui excepteur. Occaecat ipsum labore dolor excepteur duis mollit occaecat.
+                        <p className="paragraph paragraph-white u-center-text">
+                            5 Pack: $550
+                        </p>
+                        <p className="paragraph paragraph-white u-center-text">
+                            10 Pack: $1000
                         </p>
                     </div>
                 </section>
@@ -70,8 +83,14 @@ const Training = () => {
                     </div>
 
                     <div className="row">
-                        <p className="paragraph">
-                            Laboris officia sit aute id eiusmod et magna incididunt adipisicing non. Veniam id non dolore nulla ad est duis enim occaecat. Voluptate cupidatat veniam laborum excepteur pariatur ex dolore qui voluptate nisi ut cillum duis velit. Consectetur exercitation deserunt reprehenderit in. Aliquip non consequat enim ea reprehenderit.
+                        <p className="paragraph u-center-text">
+                            1 Session: $140
+                        </p>
+                        <p className="paragraph u-center-text">
+                            5 Pack: $650
+                        </p>
+                        <p className="paragraph u-center-text">
+                            10 Pack: $1100
                         </p>
                     </div>
                 </section>
